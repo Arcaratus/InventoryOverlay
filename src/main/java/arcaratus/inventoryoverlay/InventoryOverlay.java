4bc1e9dfd6828ce6f8b84d13a0e575d4469d7a2b
Arcaratus <byou64@gmail.com>
2020-11-16 18:28:14 -0500

    1.16.3 update

--- src/main/java/arcaratus/inventoryoverlay/InventoryOverlay.java
+++ src/main/java/arcaratus/inventoryoverlay/InventoryOverlay.java
@@ -1,41 +1,41 @@
 package arcaratus.inventoryoverlay;
 
-import baubles.common.container.ContainerPlayerExpanded;
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.MainWindow;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Gui;
-import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.client.gui.AbstractGui;
 import net.minecraft.client.renderer.*;
-import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.client.settings.KeyBinding;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.Slot;
+import net.minecraft.client.util.InputMappings;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.container.Slot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.vector.Matrix4f;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.settings.KeyConflictContext;
-import net.minecraftforge.client.settings.KeyModifier;
-import net.minecraftforge.common.config.Config;
-import net.minecraftforge.common.config.ConfigManager;
-import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
-import net.minecraftforge.fml.common.*;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
-import org.lwjgl.input.Keyboard;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import org.apache.commons.lang3.tuple.Pair;
+import org.lwjgl.glfw.GLFW;
 
-import java.util.Locale;
-
-@Mod(modid = InventoryOverlay.MOD_ID, name = InventoryOverlay.NAME, version = InventoryOverlay.VERSION, clientSideOnly = true, dependencies = "after:baubles@[1.5.2,)", guiFactory = "arcaratus.taboverlay.GuiOverlayConfig$Factory")
-@Mod.EventBusSubscriber(value = Side.CLIENT, modid = InventoryOverlay.MOD_ID)
+@Mod(InventoryOverlay.MOD_ID)
+@Mod.EventBusSubscriber(value = Dist.CLIENT, modid = InventoryOverlay.MOD_ID)
 public class InventoryOverlay
 {
     public static final String MOD_ID = "inventoryoverlay";
     public static final String NAME = "Inventory Overlay";
-    public static final String VERSION = "@VERSION@";
+    public static final String VERSION = "1.2.0";
 
-    private static final boolean baublesLoaded = Loader.isModLoaded("baubles");
+    private static final boolean baublesLoaded = false;//isModLoaded("baubles");
 
     private static final ResourceLocation OVERLAY = new ResourceLocation(MOD_ID, "textures/gui/overlay.png");
     private static final ResourceLocation BAUBLES_OVERLAY = new ResourceLocation(MOD_ID, "textures/gui/overlay_baubles.png");
@@ -47,15 +47,30 @@ public class InventoryOverlay
 
     public static final KeyBinding KEY_OVERLAY = new KeyBindingOverlay();
 
+    public static final ConfigHandler CLIENT;
+    public static final ForgeConfigSpec CLIENT_SPEC;
+    static
+    {
+        final Pair<ConfigHandler, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(ConfigHandler::new);
+        CLIENT_SPEC = specPair.getRight();
+        CLIENT = specPair.getLeft();
+    }
+
+    public InventoryOverlay()
+    {
+        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, CLIENT_SPEC);
+    }
+
     @SubscribeEvent
     public static void onDrawScreenPost(RenderGameOverlayEvent.Post event)
     {
-        Minecraft mc = Minecraft.getMinecraft();
-        ScaledResolution res = event.getResolution();
-        EntityPlayer player = mc.player;
-        boolean baublesOverlay = baublesLoaded && ConfigHandler.baublesOverlay;
+        Minecraft mc = Minecraft.getInstance();
+        MainWindow res = event.getWindow();
+        PlayerEntity player = mc.player;
+        MatrixStack matrixStack = event.getMatrixStack();
+        boolean baublesOverlay = baublesLoaded && ConfigHandler.baublesOverlay.get();
 
-        if (mc.currentScreen == null && Keyboard.isKeyDown(KEY_OVERLAY.getKeyCode()))
+        if (mc.currentScreen == null && KEY_OVERLAY.isKeyDown())
         {
             int i = (res.getScaledWidth() - width) / 2;
             int j = (res.getScaledHeight() - height) / 2;
@@ -63,120 +78,116 @@ public class InventoryOverlay
             zLevel = -1000;
 
             // Draw background image
-            GlStateManager.enableBlend();
-            GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
-            GlStateManager.color(1.0F, 1.0F, 1.0F, (float) ConfigHandler.overlayOpacity);
+            RenderSystem.enableBlend();
+            RenderSystem.blendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA.param, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA.param, GlStateManager.SourceFactor.ONE.param, GlStateManager.DestFactor.ZERO.param);
+            RenderSystem.color4f(1.0F, 1.0F, 1.0F, ConfigHandler.overlayOpacity.get().floatValue());
 
-            GlStateManager.pushMatrix();
-            mc.getTextureManager().bindTexture(baublesOverlay ? BAUBLES_OVERLAY : OVERLAY);
-            drawTexturedModalRect(i + ConfigHandler.xOffset, j + ConfigHandler.yOffset, 0, 0, width, height);
-            GlStateManager.popMatrix();
+            RenderSystem.pushMatrix();
+            mc.getTextureManager().bindTexture(OVERLAY);
+            drawTexturedModalRect(i + ConfigHandler.xOffset.get(), j + ConfigHandler.yOffset.get(), 0, 0, width, height);
+            RenderSystem.popMatrix();
 
             // Draw foreground
-            GlStateManager.disableRescaleNormal();
-            GlStateManager.disableDepth();
-            RenderHelper.enableGUIStandardItemLighting();
+            RenderSystem.disableRescaleNormal();
+            RenderSystem.disableDepthTest();
+            RenderHelper.enableStandardItemLighting();
 
-            GlStateManager.pushMatrix();
-            GlStateManager.translate((float)i, (float)j, 0);
+            RenderSystem.pushMatrix();
+            RenderSystem.translatef((float) i, (float) j, 0);
 
-            GlStateManager.enableRescaleNormal();
-            OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240.0F, 240.0F);
+            RenderSystem.enableRescaleNormal();
+//            OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240.0F, 240.0F);
 
-            for (int i1 = 0; i1 < player.inventoryContainer.inventorySlots.size(); ++i1)
-            {
-                Slot slot = player.inventoryContainer.inventorySlots.get(i1);
-                if (slot.isEnabled())
-                    drawSlot(mc, player, slot);
-            }
+            ItemRenderer itemRenderer = mc.getItemRenderer();
+            zLevel = 200.0F;
+            itemRenderer.zLevel = 200.0F;
+            for (Slot slot : player.container.inventorySlots)
+                if (slot.isEnabled() && !slot.getStack().isEmpty())
+                    drawSlot(mc, player, slot, itemRenderer, matrixStack);
 
-            if (baublesOverlay)
-                drawBaublesSlots(mc, player, i, j);
+            itemRenderer.zLevel = 0.0F;
+            zLevel = 0.0F;
+//            if (baublesOverlay)
+//                drawBaublesSlots(mc, player, i, j);
 
-            GlStateManager.disableAlpha();
-            GlStateManager.disableRescaleNormal();
+            RenderSystem.disableAlphaTest();
+            RenderSystem.disableRescaleNormal();
 
-            GlStateManager.popMatrix();
-            GlStateManager.enableDepth();
+            RenderSystem.popMatrix();
+            RenderSystem.enableDepthTest();
             RenderHelper.disableStandardItemLighting();
 
-            GlStateManager.color(1, 1, 1, 1); // Re-renders the food bar
-            zLevel = 0;
-            mc.getTextureManager().bindTexture(Gui.ICONS);
+            RenderSystem.color4f(1, 1, 1, 1); // Re-renders the food bar
+
+            mc.getTextureManager().bindTexture(AbstractGui.GUI_ICONS_LOCATION);
         }
     }
 
-    private static void drawSlot(Minecraft mc, EntityPlayer player, Slot slotIn)
+    private static void drawSlot(Minecraft mc, PlayerEntity player, Slot slot, ItemRenderer itemRenderer, MatrixStack matrixStack)
     {
-        int i = slotIn.xPos + ConfigHandler.xOffset;
-        int j = slotIn.yPos + ConfigHandler.yOffset;
-        ItemStack itemstack = slotIn.getStack();
-        RenderItem itemRender = mc.getRenderItem();
-        boolean flag1 = false;
+        int i = slot.xPos + ConfigHandler.xOffset.get();
+        int j = slot.yPos + ConfigHandler.yOffset.get();
+        ItemStack itemstack = slot.getStack();
+//        boolean flag1 = false;
 
-        zLevel = 100.0F;
-        itemRender.zLevel = 100.0F;
+//        if (itemstack.isEmpty() && slot.isEnabled())
+//        {
+//            com.mojang.datafixers.util.Pair<ResourceLocation, ResourceLocation> pair = slot.getBackground();
+//            if (pair != null)
+//            {
+//                TextureAtlasSprite sprite = mc.getAtlasSpriteGetter(pair.getFirst()).apply(pair.getSecond());
+//
+//                if (sprite != null && (!baublesLoaded || !sprite.getAtlasTexture().getTextureLocation().toString().equals("minecraft:items/empty_armor_slot_shield")))
+//                {
+//                    zLevel = 100.0F;
+//                    itemRenderer.zLevel = 100.0F;
+//                    RenderSystem.disableLighting();
+//                    mc.getTextureManager().bindTexture(sprite.getAtlasTexture().getTextureLocation());
+//                    blit(matrixStack.getLast().getMatrix(), i, j, i + 16, j + 16, (int) zLevel, sprite.getMinU(), sprite.getMaxU(), sprite.getMinV(), sprite.getMaxV());
+//                    RenderSystem.enableLighting();
+//                    flag1 = true;
+//                }
+//            }
+//        }
 
-        if (itemstack.isEmpty() && slotIn.isEnabled())
-        {
-            TextureAtlasSprite textureatlassprite = slotIn.getBackgroundSprite();
-
-            if (textureatlassprite != null && (!baublesLoaded || !textureatlassprite.getIconName().equals("minecraft:items/empty_armor_slot_shield")))
+//        if (!flag1)
+//        {
+//            if (baublesLoaded && ConfigHandler.baublesOverlay.get() && slot.slotNumber == 45)
+//            {
+//                itemRender.renderItemAndEffectIntoGUI(player, itemstack, i + 19, j);
+//                itemRender.renderItemOverlayIntoGUI(mc.fontRenderer, itemstack, i + 19, j, null);
+//            }
+//            else
             {
-                GlStateManager.disableLighting();
-                mc.getTextureManager().bindTexture(slotIn.getBackgroundLocation());
-                drawTexturedModalRect(i, j, textureatlassprite, 16, 16);
-                GlStateManager.enableLighting();
-                flag1 = true;
+                itemRenderer.renderItemAndEffectIntoGUI(player, itemstack, i, j);
+//                itemRenderer.renderItemOverlayIntoGUI(mc.fontRenderer, itemstack, i, j, null);
             }
-        }
-
-        itemRender.zLevel = 0.0F;
-        zLevel = 0.0F;
-
-        if (!flag1)
-        {
-            zLevel = 200.0F;
-            itemRender.zLevel = 200.0F;
-
-            if (baublesLoaded && ConfigHandler.baublesOverlay && slotIn.slotNumber == 45)
-            {
-                itemRender.renderItemAndEffectIntoGUI(player, itemstack, i + 19, j);
-                itemRender.renderItemOverlayIntoGUI(mc.fontRenderer, itemstack, i + 19, j, null);
-            }
-            else
-            {
-                itemRender.renderItemAndEffectIntoGUI(player, itemstack, i, j);
-                itemRender.renderItemOverlayIntoGUI(mc.fontRenderer, itemstack, i, j, null);
-            }
-
-            zLevel = 0.0F;
-            itemRender.zLevel = 0.0F;
-        }
+//        }
     }
 
-    @Optional.Method(modid = "baubles")
-    private static void drawBaublesSlots(Minecraft mc, EntityPlayer player, int i, int j)
-    {
-        ContainerPlayerExpanded container = new ContainerPlayerExpanded(player.inventory, !player.getEntityWorld().isRemote, player);
-        for (int i1 = 0; i1 < container.inventorySlots.size(); i1++)
-        {
-            Slot slot = container.inventorySlots.get(i1);
-            if (slot.getHasStack() && slot.getSlotStackLimit()==1)
-                drawSlot(mc, player, slot);
-        }
-    }
+//    @Optional.Method(modid = "baubles")
+//    private static void drawBaublesSlots(Minecraft mc, EntityPlayer player, int i, int j)
+//    {
+//        ContainerPlayerExpanded container = new ContainerPlayerExpanded(player.inventory, !player.getEntityWorld().isRemote, player);
+//        for (int i1 = 0; i1 < container.inventorySlots.size(); i1++)
+//        {
+//            Slot slot = container.inventorySlots.get(i1);
+//            if (slot.getHasStack() && slot.getSlotStackLimit() == 1)
+//                drawSlot(mc, player, slot);
+//        }
+//    }
 
-    private static void drawTexturedModalRect(int xCoord, int yCoord, TextureAtlasSprite textureSprite, int widthIn, int heightIn)
+    private static void blit(Matrix4f matrix, int x1, int x2, int y1, int y2, int blitOffset, float minU, float maxU, float minV, float maxV)
     {
-        Tessellator tessellator = Tessellator.getInstance();
-        BufferBuilder bufferbuilder = tessellator.getBuffer();
+        BufferBuilder bufferbuilder = Tessellator.getInstance().getBuffer();
         bufferbuilder.begin(7, DefaultVertexFormats.POSITION_TEX);
-        bufferbuilder.pos(xCoord, yCoord + heightIn, zLevel).tex(textureSprite.getMinU(), textureSprite.getMaxV()).endVertex();
-        bufferbuilder.pos(xCoord + widthIn, yCoord + heightIn, zLevel).tex(textureSprite.getMaxU(), textureSprite.getMaxV()).endVertex();
-        bufferbuilder.pos(xCoord + widthIn, yCoord, zLevel).tex(textureSprite.getMaxU(), textureSprite.getMinV()).endVertex();
-        bufferbuilder.pos(xCoord, yCoord, zLevel).tex(textureSprite.getMinU(), textureSprite.getMinV()).endVertex();
-        tessellator.draw();
+        bufferbuilder.pos(matrix, (float)x1, (float)y2, (float)blitOffset).tex(minU, maxV).endVertex();
+        bufferbuilder.pos(matrix, (float)x2, (float)y2, (float)blitOffset).tex(maxU, maxV).endVertex();
+        bufferbuilder.pos(matrix, (float)x2, (float)y1, (float)blitOffset).tex(maxU, minV).endVertex();
+        bufferbuilder.pos(matrix, (float)x1, (float)y1, (float)blitOffset).tex(minU, minV).endVertex();
+        bufferbuilder.finishDrawing();
+        RenderSystem.enableAlphaTest();
+        WorldVertexBufferUploader.draw(bufferbuilder);
     }
 
     private static void drawTexturedModalRect(int x, int y, int textureX, int textureY, int width, int height)
@@ -192,40 +203,30 @@ public class InventoryOverlay
         tessellator.draw();
     }
 
-    @SideOnly(Side.CLIENT)
     public static class KeyBindingOverlay extends KeyBinding
     {
         public KeyBindingOverlay()
         {
-            super(MOD_ID + ".keybind.overlay".toLowerCase(Locale.ENGLISH), KeyConflictContext.IN_GAME, KeyModifier.NONE, Keyboard.KEY_GRAVE, NAME);
+            super(MOD_ID + ".keybind.overlay", KeyConflictContext.IN_GAME, InputMappings.Type.KEYSYM, GLFW.GLFW_KEY_GRAVE_ACCENT, NAME);
             ClientRegistry.registerKeyBinding(this);
         }
     }
 
-    @Config(modid = MOD_ID, name = NAME + "/" + MOD_ID)
-    @Mod.EventBusSubscriber(modid = MOD_ID)
     public static class ConfigHandler
     {
-        @SubscribeEvent
-        public static void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent event)
+        public static ForgeConfigSpec.IntValue xOffset;
+        public static ForgeConfigSpec.IntValue yOffset;
+        public static ForgeConfigSpec.DoubleValue overlayOpacity;
+
+        public static ForgeConfigSpec.BooleanValue baublesOverlay;
+
+        public ConfigHandler(ForgeConfigSpec.Builder builder)
         {
-            if (event.getModID().equals(MOD_ID))
-                ConfigManager.sync(event.getModID(), Config.Type.INSTANCE);
+            builder.push("general");
+            xOffset = builder.comment("Adjusts the X-position of the overlay.").defineInRange("xOffset", 0, -1000, 1000);
+            yOffset = builder.comment("Adjusts the Y-position of the overlay.").defineInRange("yOffset", 0, -1000, 1000);
+            overlayOpacity = builder.comment("Adjusts the opacity of the overlay.").defineInRange("overlayOpacity", 0.05, 0, 1);
+            baublesOverlay = builder.comment("Uses the Baubles overlay when Baubles is in the game.").define("baublesOverlay", true);
         }
-
-        @Config.Comment({ "Adjust the X-position of the overlay." })
-        @Config.RangeInt(min = -1000, max = 1000)
-        public static int xOffset = 0;
-
-        @Config.Comment({ "Adjust the Y-position of the overlay." })
-        @Config.RangeInt(min = -1000, max = 1000)
-        public static int yOffset = 0;
-
-        @Config.Comment({ "Toggle the opacity of the overlay." })
-        @Config.RangeDouble(min = 0, max = 1)
-        public static double overlayOpacity = 0.05;
-
-        @Config.Comment({ "Disable the Baubles overlay." })
-        public static boolean baublesOverlay = true;
     }
 }
